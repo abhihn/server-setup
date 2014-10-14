2fda92f1ccf15e83bae69d34ad8b471fb507e6ee
abhihn <ahnagaraja@qwinixtech.com>
2014-10-14 16:53:21 +0530

    adding ntp

--- cookbooks/qwinix-app-setup/recipes/default.rb
+++ cookbooks/qwinix-app-setup/recipes/default.rb
@@ -29,6 +29,18 @@ template "/etc/selinux/config" do
   )
   action :nothing
 end
+#2. Configuring NTP
 
+package 'ntp' do
+action :install
+end
+
+service "ntpd" do
+ action :start
+end
+
+execute "chkconfig ntpd on" do
+ command "chkconfig ntpd on"
+end
 
 
