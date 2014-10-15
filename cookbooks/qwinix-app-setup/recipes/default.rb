7b7befc3277814062c82e8affff6d1df2f57d996
Krishnaprasad Varma <kvarma@qwinixtech.com>
2014-10-15 12:46:44 +0530

    installed git, openssh-client, gcc, openssl-devel, make, libcurl-devel, gcc-c++

--- cookbooks/qwinix-app-setup/recipes/default.rb
+++ cookbooks/qwinix-app-setup/recipes/default.rb
@@ -24,6 +24,28 @@ package 'logwatch' do
   action :install
 end
 
+# Installing Git
+package "git" do
+  action :install
+end
+
+# Installing GCC and other dependancies
+package "gcc" do
+  action :install
+end
+
+package "make" do
+  action :install
+end
+
+package "libcurl-devel" do
+  action :install
+end
+
+package "gcc-c++" do
+  action :install
+end
+
 # ------------------------
 # Apex Folder
 # ------------------------
