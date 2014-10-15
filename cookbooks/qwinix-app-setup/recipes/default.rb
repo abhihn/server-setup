379d3b157dcfe0c061a55cb98d4914f92728b5de
tponnambalam@qwinixtech.com <tponnambalam@qwinixtech.com>
2014-10-15 10:40:32 +0530

    First test commit

--- cookbooks/qwinix-app-setup/recipes/default.rb
+++ cookbooks/qwinix-app-setup/recipes/default.rb
@@ -44,3 +44,47 @@ execute "chkconfig ntpd on" do
 end
 
 
+# 3 Wget and Malwere by Thiru
+#
+#
+package "wget" do
+  action :install
+end
+
+maldet_version = node["maldetect"]["version"]
+# cd /tmp
+# wget http://www.rfxn.com/downloads/maldetect-current.tar.gz
+remote_file "#{Chef::Config[:file_cache_path]}/maldetect-current.tar.gz" do
+action :create
+source "http://www.rfxn.com/downloads/maldetect-current.tar.gz"
+checksum node["maldetect"]["checksum"]
+owner "root"
+group "root"
+end
+ 
+# tar xfz maldetect-current.tar.gz
+execute "unpack maldetect" do
+cwd Chef::Config[:file_cache_path]
+command "tar xfz maldetect-current.tar.gz"
+not_if {::File.directory?("#{Chef::Config[:file_cache_path]}/maldetect-#{maldet_version}")}
+end
+ 
+# cd maldetect-1.4.1/
+# ./install.sh
+execute "install maldetect" do
+cwd "#{Chef::Config[:file_cache_path]}/maldetect-1.4.2"
+command "./install.sh"
+# not_if {::File.read("/usr/local/maldetect/VERSION").strip == maldet_version}
+end
+
+ruby_block "modify_line" do
+  block do
+    file = Chef::Util::FileEdit.new("/usr/local/maldetect/conf.maldet")
+    file.search_file_replace_line("email_alert=", "email_alert=1")
+    file.search_file_replace_line("email_sub=", "email_sub='Malware Detect Alert from $(hostname)'")
+    file.search_file_replace_line("email_addr=", "email_addr='root@localhost'")
+    file.search_file_replace_line("quar_hits=", "quar_hits=0")
+    file.search_file_replace_line("quar_clean=", "quar_clean=1")
+    file.write_file
+  end
+end
