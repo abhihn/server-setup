e7ba64502f1173f10ad198b9ea8afdcaea5b825a
Qwinix <achannegowda@qwinixtech.com>
2014-10-16 11:31:37 +0530

    sshconfig

--- cookbooks/qwinix-app-setup/recipes/default.rb
+++ cookbooks/qwinix-app-setup/recipes/default.rb
@@ -274,4 +274,13 @@ execute "chkconfig postfix on"
 
 # Start Postfix mail daemon
 execute "service postfix start"
->>>>>>> master
+
+#Change ssh config
+ruby_block "insert_line_if_no_match" do
+  block do
+    file = Chef::Util::FileEdit.new("/etc/ssh/sshd_config")
+    file.insert_line_if_no_match("ListenAddress 0.0.0.0", "ListenAddress 0.0.0.0")
+    file.insert_line_if_no_match("PermitRootLogin no", "PermitRootLogin no")
+    file.write_file
+  end
+end
