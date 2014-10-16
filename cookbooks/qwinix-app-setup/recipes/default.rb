5d3a9ac9d3daa160c846687fafa4de346da5a1d0
tponnambalam@qwinixtech.com <tponnambalam@qwinixtech.com>
2014-10-16 13:39:23 +0530

    Banner addition

--- cookbooks/qwinix-app-setup/recipes/default.rb
+++ cookbooks/qwinix-app-setup/recipes/default.rb
@@ -274,4 +274,61 @@ execute "chkconfig postfix on"
 
 # Start Postfix mail daemon
 execute "service postfix start"
->>>>>>> master
+
+
+# Cookbook Name:: banner
+
+file "/etc/motd" do
+owner   "root"
+group   "root"
+action  :create
+content <<-MOTD.gsub(/^ {4}/, '')
+
+==========================^[[1;32mW A R N I N G^[[0m=======================================
+This computer system is the property of the Qwinix Inc. It is for authorized use only. 
+Unauthorized or improper use of this system may result in administrative disciplinary 
+action and/or civil charges/criminal penalties. By continuing to use this system you 
+indicate your awareness of and consent to these terms and conditions of use.
+
+LOG OFF IMMEDIATELY if you do not agree to the conditions stated in this warning.
+=======================================================================================
+
+  MOTD
+end
+
+file "/etc/issue" do
+owner   "root"
+group   "root"
+action  :create
+content <<-MOTD.gsub(/^ {4}/, '')
+
+==============================^[[1;32mW A R N I N G^[[0m===============================
+This computer system is the property of the Qwinix Inc. It is for authorized use only. 
+Unauthorized or improper use of this system may result in administrative disciplinary 
+action and/or civil charges/criminal penalties. By continuing to use this system you 
+indicate your awareness of and consent to these terms and conditions of use.
+
+LOG OFF IMMEDIATELY if you do not agree to the conditions stated in this warning.
+=======================================================================================
+
+
+  MOTD
+end
+
+file "/etc/issue.net" do
+owner   "root"
+group   "root"
+action  :create
+content <<-MOTD.gsub(/^ {4}/, '')
+
+==============================^[[1;32mW A R N I N G^[[0m==============================
+This computer system is the property of the Qwinix Inc. It is for authorized use only. 
+Unauthorized or improper use of this system may result in administrative disciplinary 
+action and/or civil charges/criminal penalties. By continuing to use this system you 
+indicate your awareness of and consent to these terms and conditions of use.
+
+LOG OFF IMMEDIATELY if you do not agree to the conditions stated in this warning.
+======================================================================================
+
+  MOTD
+end
